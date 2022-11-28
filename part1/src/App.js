456a80d6eb11e59cd732a8691e4c76bbb0f1ef3c
Daniel Kenney <danielkenney99@yahoo.com>
2022-11-28 14:03:41 -0800

    unicafe feedback app

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,76 @@
 import { useState } from "react";
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleClick}>{props.text}</button>
+    </div>
+  );
 };
 
-const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>;
-};
-
-const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+const StatisticsLine = (props) => {
+  const text = props.text;
+  const value = props.value;
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      <p>
+        {text} {value}
+      </p>
+    </div>
+  );
+};
+
+const Statistics = (props) => {
+  const good = props.good;
+  const neutral = props.neutral;
+  const bad = props.bad;
+
+  if (good == 0 && bad == 0 && neutral == 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <StatisticsLine text="good" value={good} />
+        <StatisticsLine text="neutral" value={neutral} />
+        <StatisticsLine text="bad" value={bad} />
+        <StatisticsLine text="all" value={good + neutral + bad} />
+        <StatisticsLine
+          text="average"
+          value={(good - bad) / (good + neutral + bad)}
+        />
+        <StatisticsLine
+          text="positive"
+          value={(good / (good + neutral + bad)) * 100}
+        />
+      </div>
+    );
+  }
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGood = () => setGood(good + 1);
+  const increaseNeutral = () => setNeutral(neutral + 1);
+  const increaseBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={increaseGood} text="good" />
+      <Button handleClick={increaseNeutral} text="neutral" />
+      <Button handleClick={increaseBad} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
