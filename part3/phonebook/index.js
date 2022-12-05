6e2e3de5bb891a5694b18747e2f7ad9386353dd7
Daniel Kenney <danielkenney99@yahoo.com>
2022-12-04 17:05:14 -0800

    work on the phonebook backend

---
+++ part3/phonebook/index.js
@@ -0,0 +1,96 @@
+const { response } = require("express")
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World!</h1>")
+})
+
+app.get("/info", (request, response) => {
+    const numberOfPeople = persons.length
+    response.send(
+        `<p>Phonebook has info for ${numberOfPeople} people</p><p>${new Date()}`
+        )
+})
+
+app.get("/api/persons", (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete("/api/persons/:id", (request,response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id === id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0 
+    ? Math.max(...persons.map(person => person.id)) 
+    : 0
+
+    return maxId + 1
+}
+
+app.post("/api/persons", (request, response) => {
+    const body = request.body;
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: "content missing"
+        })
+    }
+
+    const person = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    persons.concat(person)
+
+    response.json(persons)
+})
